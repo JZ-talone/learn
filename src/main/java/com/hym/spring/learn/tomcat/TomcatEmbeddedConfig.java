2513eb1c003dedb3c0d8df66c1d01abf4edb68bc
huangyiming <840084614@qq.com>
2020-09-14 00:18:21 +0800

    [INI]文件上传

---
+++ src/main/java/com/hym/spring/learn/tomcat/TomcatEmbeddedConfig.java
@@ -0,0 +1,28 @@
+package com.hym.spring.learn.tomcat;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author huangyiming
+ * @since 2020/9/13 23:40
+ */
+@Configuration
+public class TomcatEmbeddedConfig {
+    @Bean
+    public TomcatServletWebServerFactory tomcatEmbedded() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+        return tomcat;
+    }
+}
