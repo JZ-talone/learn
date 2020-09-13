e2da6ccee8b749aa1cc3258b7e981bf9819d6c57
huangyiming <840084614@qq.com>
2020-09-13 23:26:36 +0800

    [INI]redis scheduler 配置

---
+++ src/main/java/com/hym/spring/learn/scheduler/SchedulerTask.java
@@ -0,0 +1,22 @@
+package com.hym.spring.learn.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author huangyiming
+ * @since 2020/9/13 23:25
+ */
+@Component
+public class SchedulerTask {
+
+    private int count = 0;
+
+    @Scheduled(cron = "*/6 * * * * ?")
+    private void process() {
+        System.out.println("this is scheduler task runing  " + (count++));
+    }
+
+}
